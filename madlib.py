922ced516ff44b685961c7e56b97902d1289cfd2
Jasmine Anderson <jazz2900@gmail.com>
2018-09-10 13:19:37 -0700

    uploaded

--- madlib.py
+++ madlib.py
@@ -1,9 +1,11 @@
-#Psuedocode
+#Madlibs
 
-#List of Requirements:
+noun_input = input("Give Me a Noun:")
 
-#Inputs:
-nouns = list()
+noun_input1 = input("Give Me another Noun:")
 
-#functions:
-#select
+verb_input = input("Give Me a Verb:")
+
+adj_input = input("Give Me a Adjective:");
+
+print("Jasmine and " + str(noun_input) + " went into a " + str(noun_input1) + " until they " + str(verb_input) + " and found themselves at a " + str(adj_input) + " Park")
